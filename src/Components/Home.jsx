f9ff6d758f839916b7cfc794a82ffab4d3c9457b
mdujjal67 <mdujjal.mm67@gmail.com>
2026-06-01 14:05:05 +0600

    added user signout feature

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,9 @@
-import Login from "./Login";
-
 
 const Home = () => {
     return (
         <div>
-            <Login></Login>
+            <h1 className="text-4xl mt-10 font-bold">This is the Home page</h1>
+            <p className="text-xl font-semibold mt-5">Go to login page for login</p>
         </div>
     );
 };
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,31 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import { useState } from "react";
 
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
-        .then( result => {
-            const user = result.user;
-            console.log(user)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser)
+            })
+            .catch(error => {
+                console.log('Error:', error)
+            })
+    };
+
+    const handleSignOut = () => {
+        signOut(auth)
+        .then(result =>{
+            console.log(result)
+            setUser(null)
         })
-        .catch(error => {
+        .catch(error =>{
             console.log('Error:', error)
         })
     }
@@ -19,7 +33,14 @@ const Login = () => {
     return (
 
         <div>
-            <button onClick={handleGoogleSignIn} className="btn btn-accent hover:bg-gray-200">Google Login</button>
+            {user ? <button onClick={handleSignOut} className="btn btn-accent hover:bg-gray-200">Sign Out</button> : <button onClick={handleGoogleSignIn} className="btn btn-accent hover:bg-gray-200">Google Login</button>}
+            {user &&
+                <div>
+                    <h1 className="font-bold text-2xl mt-5">User: {user.displayName}</h1>
+                    <h1 className="font-bold text-xl mt-2">Email: {user.email}</h1>
+                    <img className="mx-auto rounded-lg" src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
